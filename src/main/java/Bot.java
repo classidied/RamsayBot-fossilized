3a9888f6d0c4287ed3270d9b4ced09bdad919131
helia <heliamazhary@yahoo.com>
2021-04-23 19:22:50 -0400

    HELP ME PLEASE

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,8 +4,11 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class Bot {
     private static JDA jda;
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception{
         System.out.println("helia do you see this");
-        jda = (JDA) new JDABuilder(AccountType.BOT);
+        jda = (JDA) new JDABuilder(AccountType.BOT)
+                .setToken("ODM1MjQ1OTU2NTk2NjI5NTI0.YIMpag.882RAbOshwq8mAnNFzXO8QSn7jU")
+                .build();
+
     }
 }
