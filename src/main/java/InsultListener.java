e664d619e090488c52a0946f4d337249f7b5a57e
classidied <claire.wang028@gmail.com>
2021-04-24 01:45:38 -0400

    fail silently

--- src/main/java/InsultListener.java
+++ src/main/java/InsultListener.java
@@ -13,20 +13,23 @@ public class InsultListener extends ListenerAdapter {
     Scanner s = null;
     public void onGuildMessageReceived (@Nonnull GuildMessageReceivedEvent event){
         String insult = "";
-        try {
-            s = new Scanner (insultList);
-            // creating arraylist to store all insults
-            ArrayList<String> insults = new ArrayList<>();
-            while (s.hasNextLine()) {
-                insults.add(s.nextLine());
-            }
-            // randomly generating a number to choose an insult
-            Random r = new Random();
-            int num = r.nextInt(insults.size());
-            insult = insults.get(num);
+        {
+            try {
+                s = new Scanner(insultList);
+                // creating arraylist to store all insults
+                ArrayList<String> insults = new ArrayList<>();
+                while (s.hasNextLine()) {
+                    insults.add(s.nextLine());
+                }
+                System.out.println(insults);
+                // randomly generating a number to choose an insult
+                Random r = new Random();
+                int num = r.nextInt(insults.size());
+                insult = insults.get(num);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
         String messageSent= event.getMessage().getContentRaw();
         if(messageSent.equalsIgnoreCase("~insult")){
