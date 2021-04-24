932f2dfb2464f3ec8e7b7e74ac93da6951da807f
classidied <claire.wang028@gmail.com>
2021-04-24 01:06:35 -0400

    SADDDDD

--- src/main/java/InsultListener.java
+++ src/main/java/InsultListener.java
@@ -4,15 +4,27 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class InsultListener extends ListenerAdapter {
     File insultList= new File ("insult.csv");
-    Scanner s= null;
+    Scanner s = null;
     public void onGuildMessageReceived (@Nonnull GuildMessageReceivedEvent event){
-        String insult="You donkey";
+        String insult = "";
         try {
             s = new Scanner (insultList);
+            // creating arraylist to store all insults
+            ArrayList<String> insults = new ArrayList<>();
+            while (s.hasNextLine()) {
+                insults.add(s.nextLine());
+            }
+            // randomly generating a number to choose an insult
+            Random r = new Random();
+            int num = r.nextInt(insults.size());
+            insult = insults.get(num);
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
