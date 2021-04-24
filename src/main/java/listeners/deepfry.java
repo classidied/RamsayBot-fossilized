e3a48230c76fd85c475f0b9544653669966597fc
helia <heliamazhary@yahoo.com>
2021-04-24 01:07:29 -0400

    deepfry

--- src/main/java/listeners/deepfry.java
+++ src/main/java/listeners/deepfry.java
@@ -1,13 +1,16 @@
 package listeners;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
 import java.io.File;
+import java.util.List;
 import java.util.Objects;
 
 public class deepfry extends ListenerAdapter {
     public void onGuildMessageReceived (@Nonnull GuildMessageReceivedEvent event){
+        List<Member> mentionedMembers= event.getMessage().getMentionedMembers();
         String messageSent= event.getMessage().getContentRaw();
         if(messageSent.equalsIgnoreCase("~deepfry")){
             File file = new File("idiotsand.png");
