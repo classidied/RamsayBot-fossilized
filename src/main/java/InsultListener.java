38407fd09b05120772902acd52d3fc3cc24c24cd
helia <heliamazhary@yahoo.com>
2021-04-23 22:05:16 -0400

    insult list

--- src/main/java/InsultListener.java
+++ src/main/java/InsultListener.java
@@ -2,12 +2,23 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class InsultListener extends ListenerAdapter {
+    File insultList= new File ("insult.csv");
+    Scanner s= null;
     public void onGuildMessageReceived (@Nonnull GuildMessageReceivedEvent event){
+        String insult="";
+        try {
+            s = new Scanner (insultList);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         String messageSent= event.getMessage().getContentRaw();
         if(messageSent.equalsIgnoreCase("~insult")){
-            event.getChannel().sendMessage("get out, you useless sack of yankee danky doodle shite!").queue();
+            event.getChannel().sendMessage(insult).queue();
             return;
         }
     }
