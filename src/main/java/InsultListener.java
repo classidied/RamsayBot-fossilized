8a9b0d78012ec1ca4d1a4234ad724d140921659b
classidied <claire.wang028@gmail.com>
2021-04-23 22:50:51 -0400

    committing non-functional functions >:(

--- src/main/java/InsultListener.java
+++ src/main/java/InsultListener.java
@@ -4,22 +4,32 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InsultListener extends ListenerAdapter {
-    File insultList= new File ("insult.csv");
-    Scanner s= null;
+    // reading in insults from file
+
+    public static void main (String[] args) throws FileNotFoundException {
+        File insultList = new File ("insults.csv");
+        Scanner s = new Scanner (insultList);
+        String delim = ",";
+
+        // reading insults into arraylist
+        ArrayList<Comparable> insults = new ArrayList<>();
+        while (s.hasNextLine()) {
+            insults.add(s.nextLine());
+        }
+        System.out.println(insults);
+    }
+
     public void onGuildMessageReceived (@Nonnull GuildMessageReceivedEvent event){
         String insult="";
-        try {
-            s = new Scanner (insultList);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
         String messageSent= event.getMessage().getContentRaw();
         if(messageSent.equalsIgnoreCase("~insult")){
             event.getChannel().sendMessage(insult).queue();
             return;
         }
     }
+
 }
