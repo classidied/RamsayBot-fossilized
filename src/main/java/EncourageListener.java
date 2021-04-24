b8b0ee0297a4df28c48a1d23b562b0a54b8efd33
classidied <claire.wang028@gmail.com>
2021-04-24 02:09:17 -0400

    THE FILE NAME WAS WRONG OH MYGOD WHY AM I DUMB

--- src/main/java/EncourageListener.java
+++ src/main/java/EncourageListener.java
@@ -2,8 +2,15 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
 
 public class EncourageListener extends ListenerAdapter {
+
+
     public void onGuildMessageReceived (@Nonnull GuildMessageReceivedEvent event){
         String messageSent= event.getMessage().getContentRaw();
         if(messageSent.equalsIgnoreCase("~encourage")){
